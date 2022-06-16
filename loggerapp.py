bc4c2b1a117621ba83803beb267f3c3bddb2bdd3
rizMehdi <ccmeall@gmail.com>
2022-06-16 09:59:30 +0200

    Update loggerapp.py

--- loggerapp.py
+++ loggerapp.py
@@ -1,13 +1,22 @@
 import streamlit as st
 st.header("Welcome to Terraform Zero")
-picture = st.camera_input("First, take a picture of the tree")
-if picture is not None:
-  st.image(picture)
 
+
+pictureready=False
 
 form = st.form("my_form")
+
+picture = form.camera_input("First, take a picture of the tree")
+if picture is not None:
+  #st.image(picture)
+  pictureready=True
+
+  
+  
+  
 loggername = form.text_input("your name (optional)")
 otherinfo = form.text_input("add any other comments here, like damage to tree etc")
 
 # Now add a submit button to the form:
-form.form_submit_button("Submit Report")
+if pictureready==True:
+  form.form_submit_button("Submit Report")
